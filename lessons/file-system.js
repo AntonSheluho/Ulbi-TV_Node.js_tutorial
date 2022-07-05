c1a6a0551222285b0f21883b3ba810cb6697f158
Anton_Shalukho <grinfrend2@gmail.com>
2022-07-05 13:23:31 +0200

    delete testFolder

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -1,15 +1,20 @@
 const path = require('path')
 const fs = require('fs')
 
-fs.mkdir(path.resolve(__dirname, 'testFolder'), (er) => {
-    try {
-        // throw new Error
-    } catch {
-        console.log('error: ')
-    } finally {
-        console.log('not error')
-        console.log('er: ', er)
-    }
-    // if (er) console.log('error: ', er);
-    // console.log('not error')
+// fs.mkdir(path.resolve(__dirname, 'testFolder'), (er) => {
+//     try {
+//         // throw new Error
+//     } catch {
+//         console.log('error: ')
+//     } finally {
+//         console.log('not error')
+//         console.log('er: ', er)
+//     }
+//     // if (er) console.log('error: ', er);
+//     // console.log('not error')
+// })
+
+fs.rmdir(path.resolve(__dirname, 'testFolder'), (er) => {
+    if (er) console.log(er);
+    console.log('delete folder')
 })

414b7a7fbfbdc08e7bbd14e23763188bb3be3429
Anton_Shalukho <grinfrend2@gmail.com>
2022-07-05 14:10:36 +0200

    mod user.json using promise and create users.txt

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -22,9 +22,41 @@ const user = `{
     "id": "1",
     "name": "Anton",
     "hello": "world"
-}`
+};`;
+const user2 = `{
+    "id": "2",
+    "name": "Darya",
+    "hello": "world"
+};`;
 
-fs.writeFile(path.resolve(__dirname, 'user.json'), user, (er) => {
-    if (er) console.log(er);
-    console.log('error writeFile')
-})
+// fs.writeFile(path.resolve(__dirname, 'user.json'),`${user}`, (er) => {
+//     if (er) console.log('error: ', er);
+//     console.log('create writeFile')
+//     fs.appendFile(path.resolve(__dirname, 'user.json'), `${user2}`,  (er) => {if (er) {console.log('error: ', er)}; console.log('append writeFile')})
+// })
+
+const writeFileFS = async (path, date) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, date, (err) => {
+            if(err) {
+                return reject(er.message)
+            }
+            resolve()
+        })
+    })
+}
+
+const appendFileFS = async (path, date) => {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(path, date, (err) => {
+            if (err) {
+                return reject(err.message)
+            }
+            resolve()
+        })
+    })
+}
+
+writeFileFS(path.resolve(__dirname, 'users.txt'), user)
+    .then(() => appendFileFS(path.resolve(__dirname, 'users.txt'), user2))
+    .catch(err => console.log(err))
